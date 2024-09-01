f0d4eff46a1cb594df149e5f3150e54bf1718d68
Tommy Xu <tommysteryy@gmail.com>
2024-08-31 23:01:29 -0700

    added real version of scripts used

--- cpbatch.sh
+++ cpbatch.sh
@@ -15,22 +15,30 @@ if [ ! -f "$file_name" ]; then
     exit 1
 fi
 
-# Copy everything in current folder to /scratch/arc
-mkdir -p $SCRATCH_DIR/local-bo-mpd
-cp -r . $SCRATCH_DIR/local-bo-mpd
+# Ask user for confirmation before replacing the current 'local-bo-mpd' folder
+read -p "Do you want to replace the current 'local-bo-mpd' folder? (y/n): " answer
+if [[ $answer == "y" ]]; then
+    # Copy everything in the current folder to /scratch/arc/local-bo-mpd
+    rm -rf $SCRATCH_DIR/local-bo-mpd
+    mkdir -p $SCRATCH_DIR/local-bo-mpd
+    cp -f -r . $SCRATCH_DIR/local-bo-mpd
+    echo "Everything in the current folder has been copied to $SCRATCH_DIR/local-bo-mpd"
+else
+    echo "$SCRATCH_DIR/local-bo-mpd has not been changed."
+fi
 
 # # Transfer the file to /scratch/arc
-# cp "$file_name" $SCRATCH_DIR
+cp -f "$file_name" $SCRATCH_DIR
 
-echo "Everything is transferred to $SCRATCH_DIR"
+echo "$file_name is transferred to $SCRATCH_DIR for running."
 
 # copy copier.sh to $SCRATCH_DIR/slurm
-cp copier.sh $SCRATCH_DIR
+cp -f copier.sh $SCRATCH_DIR
 chmod +x $SCRATCH_DIR/copier.sh
 
 echo "File copier.sh transferred to $SCRATCH_DIR"
 
-echo "Open a new terminal and cd to $SCRATCH_DIR to run the copier.sh script"
+echo "Open a new terminal and cd to $SCRATCH_DIR to run the copier.sh script, if you want SLURM output files to be copied to the current folder live."
 
 read -p "Press Enter to continue..."
 
